36a2845f1b83caa4ce58db6ca112621340cc5fe9
Matias Charrut <mcharrut@fi.uba.ar>
2021-09-17 16:38:51 -0300

    Agregado TDA Socket

---
+++ socket.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "socket.h"
+// VER LO QUE ESTABA ESCRITO EN LA CARPETA
+
+#define PENDING_CONNECTIONS 8
+
+static void _initializeSockAddrInStruct(Socket * self, int port){
+    memset(&(self->addr), 0, sizeof(struct  sockaddr_in));
+    (self->addr).sin_family = AF_INET;
+    (self->addr).sin_port = htons(port);
+}
+
+void socketInit(Socket * self){
+    self->fd = socket(AF_INET, SOCK_STREAM, 0);
+    int optval = 1;
+    setsockopt(self->fd, SOL_SOCKET, SO_REUSEADDR, &optval sizeof(int));
+}
+
+void socketInitFromFd(Socket * self, int fd){
+    self->fd = fd;
+    int optval = 1;
+    setsockopt(self->fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+}
+
+void socketUnInit(Socket * self){
+    shutdown(self->fd, SHUT_WR); // BUSCAR ESTO DE SHUT
+    close(self->fd);
+}
+
+int socketConnect(Socket * self, char * host, int port){
+    _initializeSockAddrInStruct(self, port);
+    if (inet_pton(AF_INET, host, &(self->addr).sin_addr))
+        return 1;
+    if (connect(self->fd,
+                (struct sockaddr *) &(self->addr), sizeof(struct sockaddr)))
+        return 1;
+    return 0;
+}
+
+int socketBindAndListen(Socket * self, int port){
+    _initializeSockAddrInStruct(self, port);
+    if (bind(self->fd,
+             (struct sockaddr *) &(self->addr), sizeof(struct sockaddr))) {
+        perror("");
+        return 1;
+    }
+    if (listen(self->fd, PENDING_CONNECTIONS)) {
+        perror("");
+        return 1;
+    }
+    return 0;
+}
+
+int socketAccept(Socket * self, Socket * peer){
+    socklen_t peer_addr_size = sizeof(struct sockaddr_in);
+    int fd_peer = accept(self->fd,
+                         (struct sockaddr *) &(self->addr), &peer_addr_size);
+    if (fd_peer < 0)
+        return 1;
+    socketInitFromFd(peer, fd_peer);
+    return 0;
+}
+
+
+//REVISAR FUNCIONES CON LO DEL DIPA
+int socketSend(Socket * self, char * buffer, size_t size){
+    size_t total_bytes_sent = 0;
+    while ((size - total_bytes_sent) > 0){
+        int bytes_sent = send(self->fd, &buffer[total_bytes_sent],
+                              size - total_bytes_sent, 0);
+        if (bytes_sent < 0)
+            return -1;
+        total_bytes_sent += bytes_sent;
+    }
+    return total_bytes_sent;
+}
+
+int socketReceive(Socket * peer, char * buffer, size_t size){
+    size_t total_bytes_received = 0;
+    while ((size - total_bytes_received) > 0){
+        int bytes_received =
+                recv(peer->fd, buffer[total_bytes_received],
+                     size - total_bytes_received, 0);
+        if (bytes_received < 0)
+            return -1;
+        else if (!bytes_received)
+            return total_bytes_received;
+        total_bytes_received += bytes_received;
+    }
+    return total_bytes_received;
+}
+
+
---
+++ socket.h
@@ -0,0 +1,28 @@
+//
+// Created by matias on 16/9/21.
+//
+
+#ifndef TP1_TALLER1_SOCKET_H
+#define TP1_TALLER1_SOCKET_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct {
+    int fd;
+    struct sockaddr_in addr;
+} Socket;
+
+void socketInit(Socket * self);
+void socketInitFromFd(Socket * self, int fd);
+void socketUnInit(Socket * self);
+int socketConnect(Socket * self, char * host, int port);
+int socketBindAndListen(Socket * self, int port);
+int socketAccept(Socket * self, Socket * peer);
+int socketSend(Socket * self, char * buffer, size_t size);
+int socketReceive(Socket * peer, char * buffer, size_t size);
+
+#endif //TP1_TALLER1_SOCKET_H
